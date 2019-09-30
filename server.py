e3b3d9fa05a507aac531d05025c80246ff7cc7d5
SrVladyslav <vlama@inf.upv.es>
2019-09-30 07:51:09 +0200

    F

--- server.py
+++ server.py
@@ -1,7 +1,6 @@
 import socket
 import _thread as t		
 import sys
-import os
 
 def cliente (c, addr, client):
 	print('Peticion entrante de: ', addr) 
@@ -26,36 +25,31 @@ def cliente (c, addr, client):
 			c.close()
 	print(f"===Cliente <" ,client ,f"> se ha desconectado.===")
 
+# creamos el objeto del socket 
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+print("Socket creado!")
 
-if __name__ == "__main__":
-	# declaramos el puerto en el que haremos la conexion
-    port = int(os.environ.get("PORT", 5000))
-    port1 = port
-    app.run(host='0.0.0.0', port=port)
-    server()
+# declaramos el puerto en el que haremos la conexion
+port = 23451				
 
-def server():
-	# creamos el objeto del socket 
-	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	print("Socket creado!")				
-	# ponemos a la escucha al servidor a todas las ips entrantes
-	# al puerto dado
-	s.bind(('', port))		 
-	print("socket a la escucha en el puerto: %s" %(port)) 
+# ponemos a la escucha al servidor a todas las ips entrantes
+# al puerto dado
+s.bind(('', port))		 
+print("socket a la escucha en el puerto: %s" %(port)) 
 
-	# ponemos al socket a escuchar aceptando como maximo 5 usuarios 
-	s.listen(5)	 
-	print("socket escuchando...")
-	print("==========================CONEXIONES=========================")		
-	clientes = 0
-	# bucle de escucha que acepta las peticiones
-	while clientes < 5	: 
-		# estableciendo la conexion con el cliente 
-		c, addr = s.accept()
-		t.start_new_thread(cliente, (c, addr, clientes))	 
-		clientes += 1
+# ponemos al socket a escuchar aceptando como maximo 5 usuarios 
+s.listen(5)	 
+print("socket escuchando...")
+print("==========================CONEXIONES=========================")		
+clientes = 0
+# bucle de escucha que acepta las peticiones
+while clientes < 5	: 
+	# estableciendo la conexion con el cliente 
+	c, addr = s.accept()
+	t.start_new_thread(cliente, (c, addr, clientes))	 
+	clientes += 1
 
-	# cerrando la conexion (IMPORTANTE o SUSPENDEIS) 
-	c.close()
+# cerrando la conexion (IMPORTANTE o SUSPENDEIS) 
+c.close()
 	
 
